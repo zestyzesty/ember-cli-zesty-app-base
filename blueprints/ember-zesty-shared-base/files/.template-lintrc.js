a22d0640f792af2ef97241321012b6b423d824f9
Martin Muñoz <im.mmun@gmail.com>
2016-04-29 20:02:21 -0400

    Initial blueprint

---
+++ blueprints/ember-zesty-shared-base/files/.template-lintrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+  'bare-strings': true,
+  'block-indentation': 2,
+  'deprecated-each-syntax': false
+  'html-comments': false,
+  'lint-self-closing-void-elements': false,
+  'nested-interactive': false,
+  'triple-curlies': false
+};
---
+++ blueprints/ember-zesty-shared-base/index.js
@@ -0,0 +1,39 @@
+var fs = require('fs');
+var path = require('path');
+var existsSync = require('exists-sync');
+
+function safeUnlinkSync(path) {
+  if (existsSync(path)) {
+    return fs.unlinkSync(path);
+  }
+}
+
+module.exports = {
+  name: 'ember-zesty-shared-base',
+
+  normalizeEntityName: function() {},
+
+  afterInstall: function() {
+    var blueprint = this;
+    var project = this.project
+
+    return blueprint.addPackagesToProject([
+      { name: 'ember-cli-deploy' },
+      { name: 'ember-cli-envy' },
+      { name: 'ember-cli-eslint' },
+      { name: 'ember-cli-template-lint' }
+    ]).then(function() {
+      safeUnlinkSync(path.join(project.root, '.jscsrc'));
+      safeUnlinkSync(path.join(project.root, '.jshintrc'));
+      safeUnlinkSync(path.join(project.root, 'tests/.jscsrc'));
+      safeUnlinkSync(path.join(project.root, 'tests/.jshintrc'));
+
+      return blueprint.removePackagesFromProject([
+        { name: 'ember-cli-jshint' },
+        { name: 'ember-suave' }
+      ]);
+    }).then(function() {
+      return blueprint.addAddonToProject('ember-cli-deploy-zesty-pack');
+    });
+  }
+};
