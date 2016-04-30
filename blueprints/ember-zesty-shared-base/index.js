50924fe536f208647748b6056eb1c5765b764320
Martin Muñoz <im.mmun@gmail.com>
2016-04-29 20:16:06 -0400

    Finish configuring eslint

--- blueprints/ember-zesty-shared-base/index.js
+++ blueprints/ember-zesty-shared-base/index.js
@@ -18,6 +18,7 @@ module.exports = {
     var project = this.project
 
     return blueprint.addPackagesToProject([
+      { name: 'babel-eslint' },
       { name: 'ember-cli-deploy' },
       { name: 'ember-cli-envy' },
       { name: 'ember-cli-eslint' },
