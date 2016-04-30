b304ec6ae499fe63b8a1de50bfb5fe3722c6b349
Martin Muñoz <im.mmun@gmail.com>
2016-04-30 15:06:28 -0400

    Add ember-test-selectors

--- blueprints/ember-zesty-shared-base/index.js
+++ blueprints/ember-zesty-shared-base/index.js
@@ -18,22 +18,22 @@ module.exports = {
     var project = this.project
 
     return blueprint.addPackagesToProject([
-      { name: 'babel-eslint' },
       { name: 'ember-cli-deploy' },
       { name: 'ember-cli-envy' },
       { name: 'ember-cli-eslint' },
-      { name: 'ember-cli-template-lint' }
+      { name: 'ember-cli-template-lint' },
+      { name: 'ember-test-selectors' }
     ]).then(function() {
-      safeUnlinkSync(path.join(project.root, '.travis.yml'));
-      safeUnlinkSync(path.join(project.root, '.jscsrc'));
       safeUnlinkSync(path.join(project.root, '.jshintrc'));
-      safeUnlinkSync(path.join(project.root, 'tests/.jscsrc'));
+      safeUnlinkSync(path.join(project.root, '.jscsrc'));
       safeUnlinkSync(path.join(project.root, 'tests/.jshintrc'));
-
+      safeUnlinkSync(path.join(project.root, '.jscsrc'));
+      safeUnlinkSync(path.join(project.root, '.jscsrc'));
       return blueprint.removePackagesFromProject([
-        { name: 'ember-cli-jshint' },
-        { name: 'ember-suave' }
+        'ember-cli-jshint',
+        'ember-suave'
       ]);
+      return blueprint.removePackageFromProject('ember-cli-jshint');
     }).then(function() {
       return blueprint.addAddonToProject('ember-cli-deploy-zesty-pack');
     });
