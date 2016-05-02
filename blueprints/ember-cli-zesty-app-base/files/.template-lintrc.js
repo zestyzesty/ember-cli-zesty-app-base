4ee1e9bbe69abba8a14e322ed4e502cf1bd4b203
Martin Muñoz <im.mmun@gmail.com>
2016-05-02 13:44:53 -0400

    Fix .template-lintrc.js

--- blueprints/ember-cli-zesty-app-base/files/.template-lintrc.js
+++ blueprints/ember-cli-zesty-app-base/files/.template-lintrc.js
@@ -1,9 +1,11 @@
 module.exports = {
-  'bare-strings': true,
-  'block-indentation': 2,
-  'deprecated-each-syntax': false
-  'html-comments': false,
-  'lint-self-closing-void-elements': false,
-  'nested-interactive': false,
-  'triple-curlies': false
+  rules: {
+    'bare-strings': true,
+    'block-indentation': 2,
+    'deprecated-each-syntax': false,
+    'html-comments': false,
+    'lint-self-closing-void-elements': false,
+    'nested-interactive': false,
+    'triple-curlies': false
+  }
 };
