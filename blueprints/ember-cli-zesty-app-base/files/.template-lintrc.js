a26bd5664952ee04496c13209a054e168dcfcf7b
Martin Muñoz <im.mmun@gmail.com>
2016-05-02 14:13:52 -0400

    Use recommended template linting base

--- blueprints/ember-cli-zesty-app-base/files/.template-lintrc.js
+++ blueprints/ember-cli-zesty-app-base/files/.template-lintrc.js
@@ -1,11 +1,6 @@
 module.exports = {
+  extends: 'recommended',
   rules: {
-    'bare-strings': true,
-    'block-indentation': 2,
-    'deprecated-each-syntax': false,
-    'html-comments': false,
-    'lint-self-closing-void-elements': false,
-    'nested-interactive': false,
-    'triple-curlies': false
+    'bare-strings': false
   }
 };
