336e7872ccc7e133f9e21080ffc56bfc291687dc
Martin Muñoz <im.mmun@gmail.com>
2016-06-24 18:14:41 -0400

    Update ember-concurrency

--- blueprints/ember-cli-zesty-app-base/index.js
+++ blueprints/ember-cli-zesty-app-base/index.js
@@ -17,22 +17,13 @@ module.exports = {
     var blueprint = this;
     var project = this.project;
 
-    this.insertIntoFile('ember-cli-build.js', [
-      '',
-      '    babel: {',
-      '      includePolyfill: true',
-      '    }'
-    ].join('\n'), {
-      after: '// Add options here\n'
-    });
-
     return blueprint.addPackagesToProject([
       { name: 'babel-eslint', target: '^6.0.4'},
       { name: 'ember-cli-deploy', target: '^0.6.2'},
       { name: 'ember-cli-envy', target: '^2.0.0'},
       { name: 'ember-cli-eslint', target: '^1.4.0'},
       { name: 'ember-cli-template-lint', target: '^0.4.10'},
-      { name: 'ember-concurrency', target: '^0.7.1'},
+      { name: 'ember-concurrency', target: '^0.7.8'},
       { name: 'ember-test-selectors', target: '^0.0.3'}
     ]).then(function() {
       safeUnlinkSync(path.join(project.root, '.jshintrc'));
