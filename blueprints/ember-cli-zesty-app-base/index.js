eda602900817f71203129bc00a65561d38b264e6
Martin Muñoz <im.mmun@gmail.com>
2016-11-15 10:05:11 -0500

    Update ember-cli-eslint
    It no longer requires babel-eslint. It also has support for migrating from jshint.

--- blueprints/ember-cli-zesty-app-base/index.js
+++ blueprints/ember-cli-zesty-app-base/index.js
@@ -20,7 +20,7 @@ module.exports = {
 
     return blueprint.addAddonsToProject({
       packages: [
-        { name: 'ember-cli-eslint', target: '^1.7.0' },
+        { name: 'ember-cli-eslint', target: '^2.0.0' },
         { name: 'ember-cli-template-lint', target: '^0.4.12' },
         { name: 'ember-concurrency', target: '^0.7.8' },
         { name: 'ember-test-selectors', target: '^0.0.3' },
@@ -31,18 +31,14 @@ module.exports = {
       }
     }).then(function() {
       return blueprint.addPackagesToProject([
-        { name: 'babel-eslint', target: '^6.1.2' },
         { name: 'ember-cli-deploy', target: '^0.6.4' }
       ])
     }).then(function() {
-      safeUnlinkSync(path.join(project.root, '.jshintrc'));
       safeUnlinkSync(path.join(project.root, '.jscsrc'));
-      safeUnlinkSync(path.join(project.root, 'tests/.jshintrc'));
       safeUnlinkSync(path.join(project.root, '.travis.yml'));
 
       return blueprint.removePackagesFromProject([
         { name: 'ember-ajax' },
-        { name: 'ember-cli-jshint' },
         { name: 'ember-suave' },
         { name: 'ember-welcome-page' }
       ]);
