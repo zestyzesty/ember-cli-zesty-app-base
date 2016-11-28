df03bcda5488f4f89cebee2e9fbd152d35ddc173
Ben Holmes <mail@benholmes.uk>
2016-11-28 15:19:04 -0800

    Remove ember-cli-envy

--- blueprints/ember-cli-zesty-app-base/index.js
+++ blueprints/ember-cli-zesty-app-base/index.js
@@ -41,7 +41,8 @@ module.exports = {
         { name: 'ember-ajax' },
         { name: 'ember-suave' },
         { name: 'ember-welcome-page' },
-        { name: 'babel-eslint' }
+        { name: 'babel-eslint' },
+        { name: 'ember-cli-envy' }
       ]);
     }).then(function() {
       return blueprint.addAddonToProject('ember-cli-deploy-zesty-pack');
